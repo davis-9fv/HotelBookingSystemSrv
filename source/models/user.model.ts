965f6be7c971d0b3c3c757044cd4f4b00b292344
Francisco <francisco.vinueza@alterbios.com>
2018-05-09 20:02:03 +0200

    Login

--- source/models/user.model.ts
+++ source/models/user.model.ts
@@ -4,8 +4,8 @@ var bcrypt = require('bcrypt');
 var SALT_WORK_FACTOR = 10;
 
 export interface IUser extends mongoose.Document {
-  username: string; 
-  password: string; 
+    username: string;
+    password: string;
 };
 
 export const UserSchema = new mongoose.Schema({
@@ -15,40 +15,40 @@ export const UserSchema = new mongoose.Schema({
 
 // hash the password before save
 UserSchema.pre('save', function preSaveCallback(next) {
-  // user
-  var _this = this;
+    // user
+    var _this = this;
 
-  // only hash the password if it has been modified (or is new)
-  if (!_this.isModified('password')) {
-    return next();
-  }
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function genSaltCallback(err, salt) {
-    if (err) {
-      return next(err);
+    // only hash the password if it has been modified (or is new)
+    if (!_this.isModified('password')) {
+        return next();
     }
 
-    // hash the password along with our new salt
-    bcrypt.hash(_this.password, salt, function hashCallback(err, hash) {
-      if (err) {
-        return next(err);
-      }
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function genSaltCallback(err, salt) {
+        if (err) {
+            return next(err);
+        }
 
-      // override the cleartext password with the hashed one
-      _this.password = hash;
-      next();
+        // hash the password along with our new salt
+        bcrypt.hash(_this.password, salt, function hashCallback(err, hash) {
+            if (err) {
+                return next(err);
+            }
+
+            // override the cleartext password with the hashed one
+            _this.password = hash;
+            next();
+        });
     });
-  });
 });
 
 UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function compareCallback(err, isMatch) {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, isMatch);
-  });
+    bcrypt.compare(candidatePassword, this.password, function compareCallback(err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
 };
 
 
--- source/routes/user.route.ts
+++ source/routes/user.route.ts
@@ -6,24 +6,31 @@ module.exports = (passport, router) => {
         var username = req.body.username;
         var password = req.body.password;
         console.log(req.body);
-        if(!username || !password) {
+        if (!username || !password) {
+            console.log("Username and password is required.")
             return res.status(500).send('Username and password is required.');
         } else {
             var user = new User({username: username, password: password});
             user.save();
+            console.log("User registered " + username)
             res.status(200).send('Registration successful');
         }
     });
 
     router.post('/login', (req, res, next) => {
+        console.log(req.body)
         passport.authenticate('login', (error, user) => {
-            if(error) {
+            console.log("user.route.ts: login:" + user)
+            if (error) {
+                console.log("Error:" + error)
                 res.status(500).send('ERROR');
             } else {
                 req.logIn(user, (error) => {
-                    if(error) {
+                    if (error) {
+                        console.log("Error::" + error)
                         return res.status(500).send('Request login failed');
                     } else {
+                        console.log("Free to pass")
                         return res.status(200).send('You are free to pass' + user);
                     }
                 })
@@ -31,8 +38,8 @@ module.exports = (passport, router) => {
         })(req, res, next);
     });
 
-    router.post('/logout', (req,res,next) => {
-        if(req.isAuthenticated()) {
+    router.post('/logout', (req, res, next) => {
+        if (req.isAuthenticated()) {
             req.logout();
             res.status(200).send('Logout successful');
         } else {
@@ -41,7 +48,7 @@ module.exports = (passport, router) => {
     });
 
     router.get('/greeting', (req, res, next) => {
-        if(req.isAuthenticated()) {
+        if (req.isAuthenticated()) {
             return res.status(200).send('hello!');
         } else {
             return res.status(500).send('stop that');
